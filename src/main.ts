8f8b70e8692c3213e26e6d2b4e1ab6227092675a
Sebabatso Mashego <sebabatsomashego.dev@gmail.com>
2023-04-28 19:42:28 +0200

    adjusted the parallex effect

--- src/main.ts
+++ src/main.ts
@@ -24,12 +24,25 @@ window.addEventListener('mousemove', (e: MouseEvent) => {
     // coordinates of distance from center
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
-    rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+    zValue = xValue * 1.2;
+
+    // ensures that images are always moving in the positive z-direction
+    if (Math.sign(zValue) === -1) {
+        zValue *= -1;
+    }
+
+    // setting a limit to xValue when positive
+    // to reduce the amount in which th pictures move to the right
+    if (xValue > 320) {
+        xValue = 320;
+    }
+
+    // sets a range of -40 to 18
+    // should be -40 to 40 but its 18 due to the limit set to xValue(320)
+    rotateDegree = (xValue / (window.innerWidth / 2)) * 40;
 
     parallaxImgs.forEach((element) => {
         const image = <HTMLImageElement>element;
-
-        zValue = e.clientX - parseFloat(getComputedStyle(image).left);
 
         const xRate = parseFloat(image.dataset.speedx!);
         const yRate = parseFloat(image.dataset.speedy!);
@@ -37,7 +50,7 @@ window.addEventListener('mousemove', (e: MouseEvent) => {
         const rRate = parseFloat(image.dataset.rotate!);
 
         image.style.transform = `
-        translateX(${-xValue * xRate}px) 
+        translateX(${xValue * xRate}px) 
         translateY(${yValue * yRate}px) 
         translateZ(${zValue * zRate}px)
         rotateY(${rotateDegree * rRate}deg)`;
